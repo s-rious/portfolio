f8f2ca73116a0d29a49ad95afa9845fa2ed0ac20
Cameron R <cnrpersonal@gmail.com>
2025-11-16 13:27:43 -0800

    Initial commit: fresh Next.js app

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// src/app/page.tsx
+export default function Home() {
+    return (
+        <div style={{ padding: 40 }}>
+            <h1>Your Portfolio is Working!</h1>
+            <p>Edit this file to build your homepage.</p>
+        </div>
+    );
+}
