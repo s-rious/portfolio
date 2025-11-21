96cb617b10e5a3e0fad02e9917181ef9320d5766
Cameron R <cnrpersonal@gmail.com>
2025-11-20 22:31:20 -0800

    Fix Version 0.1 runtime const export

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = 'edge';
+
 import blogPosts from '@/data/blogPosts.json';
 import { notFound } from 'next/navigation';
 import Link from 'next/link';
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = 'edge';
+
 import blogPosts from '@/data/blogPosts.json';
 import Link from 'next/link';
 
