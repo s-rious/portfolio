c2716b6b3b2927399c7040ded700f2e29a7e56db
Cameron R <cnrpersonal@gmail.com>
2025-11-20 22:38:15 -0800

    Fix Version 0.1 styles being stripped by CloudFlare

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Any other valid Next.js config options
+    experimental: {
+        optimizeCss: false,
+    }
 }
 
-export default nextConfig
+export default nextConfig;
