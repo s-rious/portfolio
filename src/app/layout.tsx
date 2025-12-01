d9b7f563183b1cb5d64cd3f1aaadcb5acca0cdf3
Cameron R <cnrpersonal@gmail.com>
2025-12-01 10:59:10 -0800

    Updated favicon ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,11 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+        <head>
+            <link rel="icon" href="/favicon.ico" />
+            <title>{title}</title>
+        </head>
+
         <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
             <Navbar />
