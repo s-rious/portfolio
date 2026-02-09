6be38ace5a437669557d12eba46d5ebd81a483b6
Cameron R <cnrpersonal@gmail.com>
2026-02-08 16:26:36 -0800

    Migrate to OpenNext for Cloudflare deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
         optimizeCss: false,
     },
+    output: 'standalone',
 }
 
 export default nextConfig;
